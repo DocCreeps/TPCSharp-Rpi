35c5b5f8cf2d18c19b6dc673bb5a7cdfd5234006
DocCreeps <dorian.vericel0546@gmail.com>
2020-10-20 12:12:59 +0200

    tp fini

--- Program.cs
+++ Program.cs
@@ -4,120 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tp1
+namespace TP1_Bases
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            do { 
-                Console.WriteLine("Bienvenue dans Computer Center que voulez vous faire ?");
-                Console.WriteLine("1: Ajouter un ordinateur, 2: Supprimer un ordinateur, 3: Rechercher un ordinateur, 4: Modifier un ordinateur, 5: Sortir");
-                string choix = Console.ReadLine();
-            
-
-
-                /* Console.WriteLine("Entrez un nombre");
-                 String saisie = Console.ReadLine();
-                 */
-                double nombre = double.Parse(choix);
-
-                Console.WriteLine("Vous avez saisie le choix N°: " + nombre);
-
-
-
-
-                Computer myFirstComputer = new Computer();
-                Computer myFirstComputer1 = new Computer();
-                myFirstComputer.Id = 1;
-                myFirstComputer.Brand = "ASUS";
-                myFirstComputer.Model = "ROGUE";
-                myFirstComputer.CPUFrequency = 2.4;
-                myFirstComputer.RAM = 800000000000;
-                myFirstComputer1.Id = 2;
-                myFirstComputer1.Brand = "ASUS";
-                myFirstComputer1.Model = "ROGUE";
-                myFirstComputer1.CPUFrequency = 2.4;
-                myFirstComputer1.RAM = 800000000000;
-                List<Computer> myList = new List<Computer>();
-                myList.Add(myFirstComputer);
-                myList.Add(myFirstComputer1);
-
-
-
-                switch (nombre)
-                {
-                    case 1:
-                        
-                        Console.WriteLine("Vous avez choisis d'ajouter un PC ");
-                        //Computer Pc = new Computer();
-                       // Pc.Id = myList.Count()+1; 
-                        Console.WriteLine("Quelle est la marque que vous voulez ajouter ? ");
-                        string brands = Console.ReadLine();
-                       // Pc.Brand = brands; 
-                        Console.WriteLine("Quelle est la modèle que vous voulez ajouter ? ");
-                        string model = Console.ReadLine();
-                       // Pc.Model = model;
-                        Console.WriteLine("Quelle est la Fréquence du CPU ? ");
-                        string CPUFS = Console.ReadLine();
-                        double CPUF = double.Parse(CPUFS);
-                       // Pc.CPUFrequency = CPUF;                        
-                        Console.WriteLine("Quelle est la quantitié de RAM ? ");
-                        string Rams = Console.ReadLine();
-                        double Ram = double.Parse(Rams);
-                       // Pc.RAM = Ram;
-
-                        myList.Add(new Computer() {Id= myList.Count()+1, Brand=brands,Model=model,CPUFrequency=CPUF,RAM=Ram });
-                        break;
-                    case 2:
-                        Console.WriteLine("Vous avez choisis de supprimer un PC ");
-                        Console.WriteLine("Quelle est l'identifiant du PC a supprimer");
-                        string Idsuppr = Console.ReadLine();
-                        int IdsupprPc = int.Parse(Idsuppr);
-                        myList.RemoveAt(IdsupprPc-1);
-                        break;
-
-                    case 3:
-                        Console.WriteLine("Quelle est l'identifiant du PC rechercher ? ");
-                        string saisie = Console.ReadLine();
-                        int identifiant = int.Parse(saisie);
-                        Console.WriteLine("Vous allez rechercher le PC avec pour identifiant: " + identifiant);
-
-                        Console.WriteLine("identifiant " + myList[identifiant - 1].Id);
-                        Console.WriteLine("Marque: " + myList[identifiant - 1].Brand);
-                        Console.WriteLine("Modèle: " + myList[identifiant - 1].Model);
-                        Console.WriteLine("Fréquence CPU: " + myList[identifiant - 1].CPUFrequency);
-                        Console.WriteLine("RAM: " + myList[identifiant - 1].RAM);
-
-
-                        break;
-                    case 4:
-                        Console.WriteLine("Vous avez choisis de modifier les caractéristiques un PC ");
-                        break;
-                    case 5:
-                        Environment.Exit(0);
-                        break;
-                    default:
-                        Console.WriteLine("Votre choix ne correspond a aucun des choix possible");
-                        break;
-                }
-
-
-
-
-
-
-
-                
-            }
-            while (Console.ReadLine()!="5");
-           
-
-            
-        }
-       
-           
-        
     public struct Computer
     {
         public long Id { get; set; }
@@ -125,8 +13,126 @@ namespace Tp1
         public string Model { get; set; }
         public double CPUFrequency { get; set; }
         public double RAM { get; set; }
- 
     }
-      
+
+
+    class Program
+    {
+        public static void displayComputer(Computer computer)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Id : " + computer.Id);
+            Console.WriteLine("Brand : " + computer.Brand);
+            Console.WriteLine("Model : " + computer.Model);
+            Console.WriteLine("CPU Frequency : " + computer.CPUFrequency);
+            Console.WriteLine("RAM : " + computer.RAM);
+        }
+
+        public static Computer setComputer(Computer computer)
+        {
+            Console.WriteLine("Brand : ");
+            computer.Brand = Console.ReadLine();
+            Console.WriteLine("Model : ");
+            computer.Model = Console.ReadLine();
+            Console.WriteLine("CPU Frequency : ");
+            computer.CPUFrequency = double.Parse(Console.ReadLine());
+            Console.WriteLine("RAM : ");
+            computer.RAM = double.Parse(Console.ReadLine());
+            return computer;
+        }
+        static void Main(string[] args)
+        {
+            Dictionary<long, Computer> Park = new Dictionary<long, Computer>();
+            long NextId;
+
+            Computer computer1 = new Computer();
+            computer1.Id = 1;
+            computer1.Brand = "Apple";
+            computer1.Model = "MacBook Pro";
+            computer1.CPUFrequency = 2.4;
+            computer1.RAM = 8;
+
+            Computer computer2 = new Computer();
+            computer2.Id = 2;
+            computer2.Brand = "Toshiba";
+            computer2.Model = "Satellite Pro";
+            computer2.CPUFrequency = 2;
+            computer2.RAM = 8;
+
+            Computer computer3 = new Computer();
+            computer3.Id = 3;
+            computer3.Brand = "Asus";
+            computer3.Model = "Zephyrus";
+            computer3.CPUFrequency = 1.2;
+            computer3.RAM = 4;
+
+            Park.Add(computer1.Id, computer1);
+            Park.Add(computer2.Id, computer2);
+            Park.Add(computer3.Id, computer3);
+
+            string saisie;
+            NextId = 4;
+
+            do
+            {
+                
+                Console.WriteLine("Que voulez-vous faire ? ");
+                Console.WriteLine("1 - Afficher le parc informatique");
+                Console.WriteLine("2 - Ajouter un ordinateur");
+                Console.WriteLine("3 - Supprimer un ordinateur");
+                Console.WriteLine("4 - Rechercher un ordinateur");
+                Console.WriteLine("5 - Modifier un ordinateur");
+                Console.WriteLine("6 - Quitter");
+
+                saisie = Console.ReadLine();
+                int choix = int.Parse(saisie);
+                switch (choix)
+                {
+                    case 1:
+                        Console.WriteLine("Parc informatique");
+                        foreach (KeyValuePair<long, Computer> idComputerPair in Park)
+                        {
+                            displayComputer(idComputerPair.Value);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Ajout d'ordinateur:");
+                        Computer newComputer = new Computer();
+                        newComputer.Id = NextId;
+                        NextId++;
+                        newComputer = setComputer(newComputer);
+                        Park.Add(newComputer.Id, newComputer);
+                        break;
+                    case 3:
+                        Console.WriteLine("Suppression d'ordinateur:");
+                        Console.WriteLine("Indiquez le numéro de l'ordinateur que vous voulez supprimer :");
+                        long IdDelete = long.Parse(Console.ReadLine());
+                        Park.Remove(IdDelete);
+                        break;
+                    case 4:
+                        Console.WriteLine("Recherche d'ordinateur:");
+                        Console.WriteLine("Quelle est l' Id ? ");
+                        long IdSearch = long.Parse(Console.ReadLine());
+                        displayComputer(Park[IdSearch]);
+                        break;
+                    case 5:
+                        Console.WriteLine("Modification d'ordinateur:");
+                        Console.WriteLine("Quelle est l' Id ? ");
+                        long IdModify = long.Parse(Console.ReadLine());
+                        Park[IdModify] = setComputer(Park[IdModify]);
+                        break;
+                    case 6:
+                        Console.WriteLine("Extinction");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Choix non reconnu, saisissez un nombre 1 et 5");
+                        break;
+                }
+            } while (choix != 6);
+
+
+            ;
+        }
     }
 }
